fb9629983731419e1ef02c3b6e930d3368464249
Guillaume Gonthier <guillaume.gonthier@strangebee.com>
2025-03-20 17:12:33 +0100

    feat(prod2-thehive elasticsearch): Enable and use ElasticSearch basic authentication

--- prod2-thehive/scripts/init.sh
+++ prod2-thehive/scripts/init.sh
@@ -18,7 +18,19 @@ SERVICE_HOSTNAME=${choice:-${SYSTEM_HOSTNAME}}
 
 
 init() {
+    ELASTICSEARCH_PASSWORD=$(cat /dev/urandom | LC_CTYPE=C tr -dc '[:alnum:]' | fold -w 64 | head -n 1)
+
     ## INIT THEHIVE CONFIGURATION
+    THEHIVEINDEXFILE="./thehive/config/index.conf"
+    THEHIVEINDEXFILETEMPLATE="./thehive/config/index.conf.template"
+    if [ ! -f ${THEHIVEINDEXFILE} ]
+    then
+        sed -e "s/###CHANGEME_ELASTICSEARCH_PASSWORD###/$ELASTICSEARCH_PASSWORD/g" < $THEHIVEINDEXFILETEMPLATE > $THEHIVEINDEXFILE
+    else
+        STATUS=1
+        warning "${THEHIVEINDEXFILE} file already exists and has not been modified."
+    fi
+
     THEHIVESECRETFILE="./thehive/config/secret.conf"
     if [ ! -f ${THEHIVESECRETFILE} ]
     then
@@ -36,8 +48,10 @@ _EOF_
     then
         CURRENT_USER_ID=$(id -u)
         CURRENT_GROUP_ID=$(id -g)
-        cat ../versions.env dot.env.template > .env
-        define_hostname # Ask user for service hostname
+        sed -e "s/###CHANGEME_ELASTICSEARCH_PASSWORD###/$ELASTICSEARCH_PASSWORD/g" < ./dot.env.template > $ENVFILE
+        cat ../versions.env >> .env
+        # Ask user for service hostname
+        define_hostname
         check_user_certificates ${SYSTEM_HOSTNAME}
         # bash $(dirname $0)/generate_certs.sh ${SYSTEM_HOSTNAME} # Generate Nginx self-signed certificates if no certificate is installed.
         cat >> ${ENVFILE} << _EOF_
